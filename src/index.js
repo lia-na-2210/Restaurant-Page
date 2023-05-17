397c2b189ec29a69accb7c9574c3eadce9738ec3
lia-na-2210 <naneko.bl@gmail.com>
2023-05-16 19:30:49 -0600

    basic settup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+
+const title = document.createElement('div');
+title.classList.add('.title');
+title.innerText = 'hello world!';
+console.log('hellow wolrd');
+
+document.body.appendChild(title);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      filename: 'index.html'
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
